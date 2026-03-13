539be8f2c3921427c7e443a1db8977cbf8fe84be
Gourav Taneja <gouravtaneja@Gouravs-MacBook-Air.local>
2026-03-13 16:36:48 +0530

    updated

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,37 +1,21 @@
 
 
+import React from "react";
 import p from "../assets/p.webp";
-
 const Hero = () => {
     return (
-        <div className="w-full flex items-center mt-10">
-
-            <div className="max-w-[1200px] mx-auto flex flex-col md:flex-row items-center justify-between px-6 w-full gap-10">
-
-                
-                <div className="order-1 md:order-2 w-full flex justify-center  lg:w-auto  ">
-                    <img
-                        src={p}
-                        alt="p"
-                        className=" w-full max-w-148 h-auto"
-                    />
-                </div>
-
-               
-                <div className="order-2 md:order-1 max-w-[490px] text-center md:text-left">
-
-                    <h1 className="font-montserrat font-bold text-[40px] md:text-[52px] leading-[120%] text-[#2B2B2B]">
-                        Built by People.
-                        <br />
-                        Backed by Tech.
+        <div className="max-w-360 m-auto w-full mt-10 mb-22.5 ">
+            <div className="px-4 lg:px-2 max-w-300 m-auto flex flex-col lg:flex-row items-center justify-between lg:gap-0 gap-10 w-full">
+                <div className="text-center lg:text-left">
+                    <h1 className="font-bold text-[36px] sm:text-[44px] lg:text-[52px] leading-[120%] text-[#2B2B2B] max-w-122.5">
+                        Built by People. Backed by Tech.
                     </h1>
 
-                    <p className="font-montserrat text-[16px] leading-[150%] text-gray-500 mt-4">
-                        We're not a software company pretending to care.<br />
-                        We're real underwriters — with real decision-making power<br />
-                        — using tech to move faster, not think for us.
+                    <p className="max-w-122.5 mt-4 font-normal text-base leading-[150%] text-[#4E4E4E]">
+                        We’re not a software company pretending to care.
+                        <br className="hidden lg:block" /> We’re real underwriters — with real decision-making power —
+                        using tech to move faster, not think for us.
                     </p>
-
                     <button className="mt-8 w-[231px] h-[60px] flex items-center justify-center rounded-full bg-[#7AC943] text-white font-medium mx-auto md:mx-0">
                         Join the Success <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g clip-path="url(#clip0_3008_8)">
@@ -45,13 +29,21 @@ const Hero = () => {
                         </svg>
 
                     </button>
-
                 </div>
 
-            </div>
+                <div className="w-full lg:w-auto min-[1360px]:translate-x-16 flex justify-center">
+                    <img
 
+                        src={p}
+                        alt="p"
+                        className="w-full max-w-148 h-auto"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Hero;
+
+

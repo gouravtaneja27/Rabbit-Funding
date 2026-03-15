28aa90b0085e957af56cd7d04c0208e4cdc240c7
Gourav Taneja <gouravtaneja@10.81.157.103>
2026-03-15 21:21:38 +0530

    updated

--- src/component/common/Button.jsx
+++ src/component/common/Button.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 
-
-const Button = ({ className, text, icon }) => {
+const Button = ({ text, icon, className }) => {
     return (
-        <button className={`cursor - pointer ${className}`}> { text }  </button >
-  )
-}
+        <button
+            className={`flex items-center gap-2 px-5 py-2 rounded-lg bg-green-500 text-white font-semibold ${className}`}
+        >
+            {icon && <span>{icon}</span>}
+            {text}
+        </button>
+    );
+};
 
-export default Button
+export default Button;
